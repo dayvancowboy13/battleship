a319b4db49d4b3b04a5ab24b20d75fa87abc4b0e
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-08-04 10:17:18 -0700

    Finish ship tests, move on to gameboard tests

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -2,7 +2,26 @@ export default class Ship {
 
     constructor() {
 
+        this.length = 1;
+        this.timesHit = 0;
+        this.sunk = false;
+
         return 'Ship created!';
+
+    }
+
+    hit() {
+
+        this.timesHit++;
+
+    }
+
+    isSunk() {
+
+        if (this.timesHit >= this.length) this.sunk = true;
+        else this.sunk = false;
+
+        return this.sunk;
 
     }
 
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -2,11 +2,26 @@ import Ship from '../src/modules/Ship.js';
 
 describe('Basic tests for ship class', () => {
 
-    test('Ship object created', () => {
-
-        let ship1 = new Ship();
+    let ship1 = new Ship();
+    test('Ship object created and has properties', () => {
 
         expect(ship1).toBeDefined();
+        expect(ship1.length).toBe(1);
+        expect(ship1.timesHit).toBe(0);
+        expect(ship1.sunk).toBe(false);
+
+        expect(ship1.hit).toBeDefined();
+        expect(ship1.isSunk).toBeDefined();
+
+    });
+
+    test('Ship method hit() increments the timesHit property, and method isSunk() properly reports isSunk property', () => {
+
+        expect(ship1.isSunk()).toBe(false);
+        expect(ship1.timesHit).toBe(0);
+        ship1.hit();
+        expect(ship1.timesHit).toBe(1);
+        expect(ship1.isSunk()).toBe(true);
 
     });
 
