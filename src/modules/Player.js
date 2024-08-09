1a621412661fe512dbbe6736fd572642a8bfc5e8
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-08-09 07:58:16 -0700

    Test Player class functionality

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,3 +1,12 @@
+import Gameboard from './Gameboard.js';
+
 export default class Player {
+
+    constructor(PlayerType) {
+
+        this.playerType = PlayerType;
+        this.gameBoard = new Gameboard();
+
+    }
 
 }
--- tests/player.test.js
+++ tests/player.test.js
@@ -1,1 +1,24 @@
 import Player from '../src/modules/Player.js';
+
+let p1 = new Player('human');
+let p2 = new Player('computer');
+
+test('Player class is defined', () => {
+
+    expect(p1).toBeDefined();
+
+});
+
+test('Player class has \'playerType\' property', () => {
+
+    expect(p1.playerType).toBeDefined();
+
+});
+
+test('Each Player class has its own Gameboard', () => {
+
+    expect(p1.gameBoard).toBeDefined();
+    expect(p2.gameBoard).toBeDefined();
+    expect(p1.gameBoard === p2.gameBoard).toBe(false);
+
+});
