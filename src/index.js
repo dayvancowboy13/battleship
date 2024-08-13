5c087fea78f93e181450a3d463761cce262f8059
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-08-13 11:20:37 -0700

    Fully implement event-driven game flow
    The game can now be played through a full (boring) round.
    Now move on to adding functionality for the player(s) to place their
    ships on their own.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ console.log('Begin!');
 
 initListeners();
 
-
+// Setup game /////
 const p1 = new Player('human');
 const p2 = new Player('computer');
 
@@ -40,18 +40,7 @@ document.dispatchEvent(new CustomEvent('begin-player-turn', {
         message: 'players have been created; array things created; boards set and rendered'
     }
 }));
-
-
-// Approach: setup event listeners for the end of each "phase"?
-
-// Setup complete -- Now player's turn
-// comprised of:
-// 1). changing the HTML to indicate Player's turn
-// 2). activating the listener for the player clicking on the enemy board
-// Player's turn over; now enemy's turn
-
-// Player turn start:
-// DOMFunctions.updatePlayerTurnHTML('human')
+// ///////////////////////
 
 function initListeners() {
 
@@ -74,15 +63,45 @@ function initListeners() {
 
         console.log('changing to enemy turn');
         document.removeEventListener('click', clickEnemyBoard);
+        // enemy move:
+        // get two random numbers from 0-9
+        // if that square has already been hit, redo it
+        console.log('simulating enemy turn');
+
         setTimeout(() => {
 
-            console.log('simulating enemy turn');
-            this.dispatchEvent(new CustomEvent('begin-player-turn', {
-                bubbles: true,
-                detail: 'game not over'
-            }));
+            let randomRow;
+            let randomCol;
+            do {
 
-        }, 5000);
+                randomRow = Math.floor(Math.random() * 10);
+                randomCol = Math.floor(Math.random() * 10);
+
+                console.log(`row: ${randomRow}; col: ${randomCol}`);
+
+            } while (p1.gameBoard.boardArray[randomRow][randomCol].isHit);
+
+            p1.gameBoard.receiveAttack([randomRow, randomCol]);
+
+            DOMFunctions.renderBoard('player', p1.gameBoard.boardArray);
+
+            if (p1.gameBoard.allShipsSunk()) {
+
+                this.dispatchEvent(new CustomEvent('game-over', {
+                    bubbles: true,
+                    detail: { winner: 'Computer Player' }
+                }));
+
+            } else {
+
+                this.dispatchEvent(new CustomEvent('begin-player-turn', {
+                    bubbles: true,
+                    detail: 'game not over'
+                }));
+
+            }
+
+        }, 1500);
 
 
     });
@@ -106,17 +125,7 @@ function initListeners() {
 
     });
 
-    document.addEventListener('keypress', (e) => {
-
-        if (e.key === 'e') {
-
-
-        }
-
-    });
-
 }
-
 
 function clickEnemyBoard(e) {
 
